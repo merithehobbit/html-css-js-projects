5f0cb944b3dca1aa19c375478f2af081f43cecfc
merithehobbit <merjemkadric2009555@gmail.com>
2026-04-24 21:43:20 +0200

    Create script.js

---
+++ filter-fun/script.js
@@ -0,0 +1,18 @@
+const upload = document.getElementById("upload");
+const preview = document.getElementById("preview");
+
+upload.addEventListener("change", function () {
+  const file = upload.files[0];
+  if (!file) return;
+
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    preview.src = e.target.result;
+  };
+
+  reader.readAsDataURL(file);
+});
+
+function setFilter(filter) {
+  preview.style.filter = filter;
+}
